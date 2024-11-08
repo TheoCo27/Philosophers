6e9b3bce537c9ddcceee48e00831925125837344
TheoCo27 <theocohen27@gmail.com>
2024-11-08 15:15:57 +0100

    safe function edit

--- src/safe.c
+++ src/safe.c
@@ -6,7 +6,7 @@
 /*   By: tcohen <tcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:09:08 by tcohen            #+#    #+#             */
-/*   Updated: 2024/11/02 20:10:39 by tcohen           ###   ########.fr       */
+/*   Updated: 2024/11/08 15:15:43 by tcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,19 @@ int	safe_read(int *to_read, pthread_mutex_t *lock)
 	result = *to_read;
 	pthread_mutex_unlock(lock);
 	return (result);
+}
+
+int	safe_edit(int *to_edit, int update, pthread_mutex_t *lock)
+{
+	pthread_mutex_lock(lock);
+	*to_edit = update;
+	pthread_mutex_unlock(lock);
+	return (update);
+}
+
+int safe_speak(char *to_say, pthread_mutex_t *lock)
+{
+	
+	pthread_mutex_lock(lock);
+	printf("")
 }
--- src/watcher.c
+++ src/watcher.c
@@ -6,7 +6,7 @@
 /*   By: tcohen <tcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:02:06 by tcohen            #+#    #+#             */
-/*   Updated: 2024/11/02 20:12:29 by tcohen           ###   ########.fr       */
+/*   Updated: 2024/11/08 14:52:09 by tcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,11 @@ void	watch_philos(t_table *table)
 		i = 0;
 		while(table->philo[i])
 		{
-			if ()
+			if (table->philo[i]->last_meal_time > table->time_die)
+			{
+				table->status = DINNER_OVER;
+			}
+			
 		}
 	}
 }
